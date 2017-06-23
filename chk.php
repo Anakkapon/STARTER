ebf5d5d0b394a93a0d69f5ec00e1a0a8154ce799
Anakkapon <sanakkapon@gmail.com>
2017-06-23 17:56:11 +0700

    Update chk.php

--- chk.php
+++ chk.php
@@ -1,16 +1,12 @@
 <?php
 require("phpMQTT.php");
-
-$client = new Mosquitto\Client();
 	
 $mqtt = new phpMQTT("www.km1.io", 1883, "phpMQTT Pub Example");
-if(!$mqtt->$mqtt->connect(true,NULL, $username = 'anakkapon', $password = 'ubgawoik')){
+if(!$mqtt->connect(true,Null, $username = 'anakkapon', $password = 'ubgawoik')){
 	exit(1);
 }
-
 $topics['/anakkapon/room507'] = array("qos"=>0, "function"=>"procmsg");
 $mqtt->subscribe($topics,0);
-
 while($mqtt->proc()){
 		
 }
