a8911c3a71598c40a2c13ce50826ccb4ac24f039
Anakkapon <sanakkapon@gmail.com>
2017-06-23 17:32:04 +0700

    Create Mosp.php

---
+++ Mosp.php
@@ -0,0 +1,25 @@
+<?php
+$client = new Mosquitto\Client();
+$client->onConnect('connect');
+$client->onDisconnect('disconnect');
+$client->onSubscribe('subscribe');
+$client->onMessage('message');
+$client->connect("localhost", 1883, 5);
+$client->onLog('logger');
+$client->subscribe('/#', 1);
+$client->loopForever();
+function connect($r) {
+	echo "I got code {$r}\n";
+}
+function subscribe() {
+	echo "Subscribed to a topic\n";
+}
+function message($message) {
+	printf("Got a message on topic %s with payload:\n%s\n", $message->topic, $message->payload);
+}
+function disconnect() {
+	echo "Disconnected cleanly\n";
+}
+function logger() {
+	var_dump(func_get_args());
+}
